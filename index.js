6b3e190a2f2325bcaa14d0c7dd9a21f266d3509a
oyama-appirits <oyama@appirits.com>
2021-04-14 14:14:04 +0900

    index.js パス変更してみた

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = protect(
   '/',
   (username, password) => safeCompare(username, 'admin') && safeCompare(password, 'admin'),
   {
-    directory: __dirname + '/out',
+    directory: __dirname + '/out/_next/static',
     realm: 'vercel-basic-auth.node-static-auth',
     onAuthFailed: res => {
       res.end('Restricted area, please login (admin:admin).');
